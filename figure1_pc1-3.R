82a3e03ab988555a7686bf5241792ef5109edafc
Jenni Shearston <jenni.shearston@gmail.com>
2019-04-25 18:45:50 -0400

    save pc1-3 constituents object and create figure 1 r script

---
+++ figure1_pc1-3.R
@@ -0,0 +1,10 @@
+## Figure 1 for Final Project - PC1-3 Constituents and Splines
+## J. Shearston
+## April 25, 2019
+
+library(tidyverse)
+library(patchwork)
+
+# Load PC1-3 Constituents figure and PC1-3 spline figure
+load(file = "./figs_tables_final_paper/figure_1a.RData")
+load(file = "./figs_tables_final_paper/figure_1b.RData")
